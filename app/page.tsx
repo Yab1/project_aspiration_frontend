d6c9337679ec42381dca5de15080132443c9c7dc
Yab1 <yabilisanu@gmail.com>
2024-07-10 17:20:40 +0300

    feat: feat: add prefetching of feedbacks data using React Query in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,122 +1,42 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
+import { FeedbackForm, Feedbacks } from "@/components/feedback/";
+import { FeedbackType } from "@/components/feedback/Feedbacks";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { axiosInstance } from "@/utils";
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
-import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from "@/components/ui/form";
-import { Textarea } from "@/components/ui/textarea";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { RotateCcw } from "lucide-react";
-import { useRef } from "react";
-import { useForm } from "react-hook-form";
-import SignatureCanvas from "react-signature-canvas";
-import { z } from "zod";
+	HydrationBoundary,
+	QueryClient,
+	dehydrate,
+} from "@tanstack/react-query";
 
-const formSchema = z.object({
-	comment: z.string().min(2).max(255),
-	signature: z.string(),
-});
-
-export default function Home() {
-	const sigCanvas = useRef<SignatureCanvas>(null);
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			comment: "",
+export default async function Home() {
+	const queryClient = new QueryClient();
+	await queryClient.prefetchQuery({
+		queryKey: ["feedbacks"],
+		queryFn: async () => {
+			try {
+				const response = await axiosInstance.get("feedbacks/");
+				return response.data as FeedbackType[];
+			} catch (error) {
+				throw new Error(`Error fetching feedbacks: ${error}`);
+			}
 		},
 	});
-
-	const handleCanvasChange = () => {
-		if (sigCanvas.current) {
-			const signature: string = sigCanvas.current
-				.getTrimmedCanvas()
-				.toDataURL("image/png");
-			form.setValue("signature", signature);
-		}
-	};
-
-	function onSubmit(values: z.infer<typeof formSchema>) {
-		console.log(values);
-	}
-
 	return (
-		<main className="flex justify-center items-center h-full">
-			<Card className="w-[600px]">
-				<CardHeader className="pb-4">
-					<CardTitle>Feedback Form</CardTitle>
-					<CardDescription>
-						We value your feedback! Please take a moment to share your thoughts with
-						us.
-					</CardDescription>
-				</CardHeader>
-				<Form {...form}>
-					<form onSubmit={form.handleSubmit(onSubmit)}>
-						<CardContent className="space-y-3">
-							<FormField
-								control={form.control}
-								name="comment"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Your Comment</FormLabel>
-										<FormControl>
-											<Textarea placeholder="Type your feedback here..." {...field} />
-										</FormControl>
-										<FormMessage className="form-error-message" />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="signature"
-								render={() => (
-									<FormItem className="flex-1">
-										<FormLabel>Signature Pad</FormLabel>
-										<FormControl>
-											<div className="relative">
-												<Button
-													size="icon"
-													variant="ghost"
-													type="button"
-													className="absolute right-2 top-1 "
-													onClick={() => sigCanvas.current?.clear()}
-												>
-													<RotateCcw size={18} />
-												</Button>
-												<SignatureCanvas
-													ref={sigCanvas}
-													canvasProps={{
-														width: 545,
-														height: 170,
-														className: "signature-pad",
-													}}
-													onEnd={handleCanvasChange}
-												/>
-											</div>
-										</FormControl>
-										<FormMessage className="form-error-message" />
-									</FormItem>
-								)}
-							/>
-						</CardContent>
-						<CardFooter>
-							<Button type="submit">Submit</Button>
-						</CardFooter>
-					</form>
-				</Form>
-			</Card>
+		<main className="h-full flex justify-center items-center">
+			<Tabs defaultValue="feedbacks" className="w-fit">
+				<TabsList className="grid w-full grid-cols-2">
+					<TabsTrigger value="feedbacks">Feedbacks</TabsTrigger>
+					<TabsTrigger value="feedback_form">Form</TabsTrigger>
+				</TabsList>
+				<TabsContent value="feedbacks">
+					<HydrationBoundary state={dehydrate(queryClient)}>
+						<Feedbacks />
+					</HydrationBoundary>
+				</TabsContent>
+				<TabsContent value="feedback_form">
+					<FeedbackForm />
+				</TabsContent>
+			</Tabs>
 		</main>
 	);
 }
