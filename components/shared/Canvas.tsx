429d952d23dd86d3a77f5a0b976c9ba0ba5b8699
Yab1 <yabilisanu@gmail.com>
2024-07-11 22:41:16 +0300

    feat: implement Canvas component for drawing and image handling

---
+++ components/shared/Canvas.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useCanvas } from "@/hooks";
+import { ImageDataType } from "@/types";
+import { RotateCcw, Upload } from "lucide-react";
+import { useEffect, useRef } from "react";
+import SignatureCanvas from "react-signature-canvas";
+import { Button } from "../ui/button";
+
+type CanvasProps = {
+	id: string;
+	onImageDataChange: (id: string, imageData: ImageDataType) => void;
+};
+
+export default function Canvas({ id, onImageDataChange }: CanvasProps) {
+	const {
+		imageData,
+		drawingMode,
+		canvasRef,
+		handleCanvasChange,
+		clearCanvas,
+		handleFileChange,
+	} = useCanvas();
+
+	const inputRef = useRef<HTMLInputElement>(null);
+
+	useEffect(() => {
+		onImageDataChange(id, imageData);
+	}, [imageData]);
+
+	return (
+		<div className="relative bg-white rounded-md">
+			<input
+				type="file"
+				hidden
+				ref={inputRef}
+				onChange={(e) => handleFileChange(e.target.files?.[0], id)}
+			/>
+			<Button
+				size="icon"
+				variant="ghost"
+				type="button"
+				className="absolute left-2 top-1 "
+				onClick={() => {
+					inputRef.current?.click();
+				}}
+			>
+				<Upload size={18} />
+			</Button>
+			<Button
+				size="icon"
+				variant="ghost"
+				type="button"
+				className="absolute right-2 top-1 "
+				onClick={clearCanvas}
+			>
+				<RotateCcw size={18} />
+			</Button>
+			{drawingMode ? (
+				<SignatureCanvas
+					ref={canvasRef}
+					canvasProps={{
+						width: 500,
+						height: 300,
+						className: "signature-pad",
+					}}
+					penColor="blue"
+					onEnd={() => handleCanvasChange(id)}
+				/>
+			) : (
+				<div className="w-[500px] h-[300px] signature-pad flex justify-center items-center">
+					<img src={imageData.blobURL ? imageData.blobURL : ""} />
+				</div>
+			)}
+		</div>
+	);
+}
